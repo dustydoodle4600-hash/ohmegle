61c2ffcdf0505514df278a914eab34f6ff641255
dustydoodle4600-hash <dustydoodle4600@gmail.com>
2026-01-14 18:26:10 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
-import { Box, Button, Grid, Text, useColorModeValue } from "@chakra-ui/react";
+import { Box, Grid, useColorModeValue } from "@chakra-ui/react";
 import Head from "next/head";
 import { FormEvent, useContext, useState } from "react";
 import Chat from "../components/Chat";
 import Header from "../components/Header";
-import Intro from "../components/Intro";
 import { PusherContext } from "../context/pusherContext";
 
 export default function Home() {
   const backgroundColor = useColorModeValue("orange.50", "gray.800");
   const [redirectToChat, setRedirectToChat] = useState(false);
-
   const { setStartPusher } = useContext(PusherContext);
 
   async function onSubmit(event: FormEvent) {
     event.preventDefault();
     setStartPusher(true);
-
     setRedirectToChat(true);
   }
 
@@ -24,18 +21,14 @@ export default function Home() {
       <Head>
         <title>OmeDEV: Talk to developers!</title>
         <link rel="icon" href="/icon.png" type="image/png" />
-        <meta
-          name="description"
-          content="The Internet is full of people. Ohmegle lets you meet them. When you use Ohmegle, we pick someone else at random so you can have a one-on-one chat."
-        />
       </Head>
-      <Grid
-        templateRows="max-content 1fr"
-        minHeight="100vh"
-        backgroundColor={backgroundColor}
-      >
+      <Grid templateRows="max-content 1fr" minHeight="100vh" backgroundColor={backgroundColor}>
         <Header />
-        {redirectToChat ? <Chat /> : <Intro onSubmit={onSubmit} />}
+        {redirectToChat ? <Chat /> : (
+          <Box display="flex" alignItems="center" justifyContent="center" height="100%">
+            <button onClick={onSubmit}>Start Chat</button>
+          </Box>
+        )}
       </Grid>
     </>
   );
